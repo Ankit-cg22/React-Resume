8ba4102856a35a867cbd3c03baf0d7ca62921bae
Ankit-cg22 <ankitdas.cg22@gmail.com>
2023-10-08 13:29:31 +0530

    Converted HOC into function based component

--- src/components/HOC/HOC.js
+++ src/components/HOC/HOC.js
@@ -1,31 +1,28 @@
-import React from 'react'
+import React from 'react';
 import BulletPoints from '../BulletPoints/BulletPoints';
-import '../../css/index.css'
+import '../../css/index.css';
 
-const HOC = (SubTitleComponent , SideInfoComponent) => {
-    return class extends React.Component{
-       
-        render(){
-            const { title, subTitleInfo, sideInfo, bulletPoints} = this.props;
+const HOC = (SubTitleComponent, SideInfoComponent) => {
+  return function HOCComponent(props) {
+    const { title, subTitleInfo, sideInfo, bulletPoints } = props;
 
-            return (
-                <div className='hoc-main-container'>
-                <div className='hoc-top-container'>
-                    <div className='experience-title-subTitle'>
-                        <div className='hoc-title'>{title}</div>
-                        <div className='hoc-subtitle'>
-                            <SubTitleComponent data={subTitleInfo}/>
-                        </div>
-                    </div>
-                    <div className='hoc-wrapped-component'>
-                        <SideInfoComponent data={sideInfo}/>
-                    </div>
-                </div>
-                <BulletPoints bulletPoints={bulletPoints}/>
-                </div>
-            )
-        }
-    }
-}
+    return (
+      <div className='hoc-main-container'>
+        <div className='hoc-top-container'>
+          <div className='experience-title-subTitle'>
+            <div className='hoc-title'>{title}</div>
+            <div className='hoc-subtitle'>
+              <SubTitleComponent data={subTitleInfo} />
+            </div>
+          </div>
+          <div className='hoc-wrapped-component'>
+            <SideInfoComponent data={sideInfo} />
+          </div>
+        </div>
+        <BulletPoints bulletPoints={bulletPoints} />
+      </div>
+    );
+  };
+};
 
-export default HOC ;
+export default HOC;
